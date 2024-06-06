67dcb15b1f1ff6421ebc58b3721409d73fa638d6
Mariia <mariia.bieskova@gmail.com>
2024-06-06 13:43:34 +0300

    add logic to check for the win and check for the tie and display a message

--- script.js
+++ script.js
@@ -19,10 +19,12 @@ const Gameboard = (() => {
         render();
     }
     
+    const getGameboard = () => gameboard;
 
     return {
         render,
         update,
+        getGameboard,
     }
 
 })();
@@ -56,22 +58,78 @@ const Game = (() =>{
     }
 
     const handleClick = (e) => {
-       const index = parseInt(e.target.id);
-       Gameboard.update(index, players[currentPlayerIndex].mark);
-       currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+       let index = parseInt(e.target.id);
+      
+
+       if (Gameboard.getGameboard()[index] !== "")
+        return; 
+
+        Gameboard.update(index, players[currentPlayerIndex].mark);
+
+        if(checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)){
+            gameover = true;
+            alert(`${players[currentPlayerIndex].name} won`)
+        }
+        else if (checkForTie(Gameboard.getGameboard())){
+            gameOver = true;
+            alert(`It's a tie!`)
+        }
+
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+    const restart = () => {
+        for (let i = 0; i < 9; i++){
+            Gameboard.update(i, "");
+        }
+        Gameboard.render();
+
     }
 
     return {
     start,
+    restart,
     handleClick,
     }
 } )();
 
+function checkForWin(board) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let i = 0; i < winningCombinations.length; i++){
+        const [a, b, c] = winningCombinations[i];
+        if(board[a] && board[a] === board[b] && board[a] === board[c]){
+            return true;
+        }
+
+        else return false;
+
+    }
+
+}
+
+function checkForTie(board){
+    return board.every(cell => cell !== "")
+}
+ 
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
    Game.start();
 })
 
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", () => {
+    Game.restart();
+})
 
 
